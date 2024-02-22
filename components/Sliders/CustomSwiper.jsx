3f78f10aee90cf31939f68d78e20f30451478196
BehzodbekImomov <behzodbeki65@gmail.com>
2024-02-22 12:59:15 +0500

    carousel o'zgardi

--- components/Sliders/CustomSwiper.jsx
+++ components/Sliders/CustomSwiper.jsx
@@ -12,10 +12,11 @@ import { Button, CircularProgress } from "@mui/material";
 import Link from "next/link";
 import { carContext } from "@/context/CarContext";
 import Loading from "@/app/[locale]/(public)/loading";
+import { useTranslation } from "react-i18next";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import "./styles.scss";
-import { REST } from "@/constants/enpoint";
-import { useTranslation } from "react-i18next";
+import { Carousel } from "react-responsive-carousel";
 
 // import required modules
 
@@ -32,67 +33,58 @@ export default function CustomSwiper({ popular, loading }) {
       {loading ? (
         <Loading />
       ) : (
-        <Swiper
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={"auto"}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          // pagination={true}
-
+        <Carousel
+          showArrows={true}
+          infiniteLoop={true}
+          dynamicHeight={true}
+        
+          showIndicators={false}
           modules={[EffectCoverflow]}
           className="mySwiper"
-          // onLoad={true}
+         
         >
           {popular.map((e) => (
-            <SwiperSlide key={e.id}>
-              <li
-                style={{
-                  backgroundImage: `url(https://backend.intechs.uz/car/v1/image/${e?.image[0]?.id})`,
-                }}
-              >
-                <div className="head_card">
-                  <p>{e?.brand}</p>
+            <li
+              key={e.id}
+              style={{
+                backgroundImage: `url(https://backend.intechs.uz/car/v1/image/${e?.image[0]?.id})`,
+              }}
+            >
+              <div className="head_card">
+                <p>{e?.brand}</p>
 
-                  <CustomizedRating />
-                </div>
-                <div className="food_card">
-                  <p>
-                    AED{e?.price_arab}/ <span>day</span>
-                  </p>
-                  <Link href={`order/${e?.id}`}>
-                    {" "}
-                    <Button
-                      loading={loading}
-                      loadingIndicator={
-                        <CircularProgress color="secondary" size={20} />
-                      }
-                      loadingPosition="end"
-                      onClick={() =>
-                        dispatch({
-                          type: "add-to-cart",
-                          payload: e?.id,
-                          products: popular,
-                        })
-                      }
-                      type="submit"
-                      variant="contained"
-                      style={{ background: "#FEC31D" }}
-                    >
-                      {t("car_button")}
-                    </Button>
-                  </Link>
-                </div>
-              </li>
-            </SwiperSlide>
+                <CustomizedRating />
+              </div>
+              <div className="food_card">
+                <p>
+                  AED{e?.price_arab}/ <span>day</span>
+                </p>
+                <Link href={`order/${e?.id}`}>
+                  {" "}
+                  <Button
+                    loading={loading}
+                    loadingIndicator={
+                      <CircularProgress color="secondary" size={20} />
+                    }
+                    loadingPosition="end"
+                    onClick={() =>
+                      dispatch({
+                        type: "add-to-cart",
+                        payload: e?.id,
+                        products: popular,
+                      })
+                    }
+                    type="submit"
+                    variant="contained"
+                    style={{ background: "#FEC31D" }}
+                  >
+                    {t("car_button")}
+                  </Button>
+                </Link>
+              </div>
+            </li>
           ))}
-        </Swiper>
+        </Carousel>
       )}
     </>
   );
